ba5f4344afac74ceb25b9096069f9ed24937ac20
tamalesimon <tamalesp@gmail.com>
2025-04-24 14:09:12 +0300

    fix: Update Dockerfile to expose the correct port and modify SQL seed data

--- dockerfile
+++ dockerfile
@@ -2,4 +2,4 @@ FROM openjdk:21-slim
 WORKDIR /app
 COPY target/auth-service-0.0.1-SNAPSHOT.jar auth-service.jar
 ENTRYPOINT ["java", "-Dspring.profiles.active=docker", "-jar", "auth-service.jar"]
-EXPOSE 8006
+EXPOSE 8082
--- src/main/resources/seed/data.sql
+++ src/main/resources/seed/data.sql
@@ -1,4 +1,4 @@
--- Active: 1744738446853@@127.0.0.1@5001@db
+-- Active: 1745360102568@@127.0.0.1@5432@postgres
 CREATE TABLE IF NOT EXISTS "users" (    id UUID PRIMARY KEY,
                                         email VARCHAR(255) NOT NULL UNIQUE,
                                         password VARCHAR(255) NOT NULL,
@@ -11,7 +11,7 @@ INSERT INTO users (id, email, password, role, status, created_at)
 VALUES (
     'c8cc86c7-26a6-44d0-8825-56f8b9a44b55',
     'john.doe@example.com',
-    '$2a$10$WqDdPUKh5xLQIZcMKZJnAeRqkUp6Gagnt63aStXPxR.cPzQ96J6Ui',
+    '$2a$12$bmZcgo7jRbjodHj3lP4nCun8QAQnct/B1LP6dFfc7H8JjGNjfjtdO',
     'SUPER_ADMIN',
     'ACTIVE',
     Now()
